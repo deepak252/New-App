e773f63528e367f85706fac3b02cd47272f72db8
deepak <trojanhorsedc@gmail.com>
2021-11-21 22:10:32 +0530

    Infite scrolling added(react-infinite-scroll-component)

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import "./News.css";
 // import Articles from "./articles";
-
+import InfiniteScroll from "react-infinite-scroll-component";
+import Spinner from "./Spinner";
 
 require("dotenv").config();
 
@@ -33,27 +34,25 @@ class News extends Component{
         })
     }
 
-    handlePrev = async () => {
-        this.setState({
-            page: this.state.page > 1 ? this.state.page - 1 : this.state.page
-        },async ()=>{
-            await this.fetchNews();
-        });
-    }
-
-    handleNext = async () =>{
-        this.setState({
-            page: this.state.page < this.state.maxPage ? this.state.page + 1 : this.state.page
-        }, async () => {
-            await this.fetchNews();
-        });
-    }
-    
     async componentDidMount(){
         this.setState({
             maxPage: Math.ceil(this.state.totalArticles/this.state.pageSize)
         });
         await this.fetchNews();
+    }
+
+    fetchMoreData = async ()=>{
+        this.setState({
+            page: this.state.page < this.state.maxPage ? this.state.page + 1 : this.state.page
+        }, async () => {
+            let url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=${process.env.REACT_APP_API_KEY1}&page=${this.state.page}&pageSize=${this.state.pageSize}`;
+            let data = await fetch(url);
+            let parsedData = await data.json();
+            this.setState({
+                articles: this.state.articles.concat(parsedData.articles),
+                totalArticles: parsedData.totalResults
+            })
+        });
     }
 
 
@@ -63,20 +62,21 @@ class News extends Component{
                 {
                     !this.state.loading
                     ? <div className="news-container">
-                            <h5>{this.state.totalArticles} {this.props.category} Articles</h5>
+                        <h5>{this.state.totalArticles} {this.props.category} Articles</h5>
+                        <InfiniteScroll
+                            dataLength={this.state.articles.length}
+                            next={this.fetchMoreData}
+                            hasMore={this.state.articles.length!=this.state.totalArticles}
+                            loader={<Spinner />}
+                        >
                         {
-                                // this.state.articles
                                 this.state.articles.map((article)=>{
                                 return <NewsItem key = {article.url} news={article} />
                             })
                         }
-                        <div className="btn-container">
-                            <button disabled ={this.state.page <= 1} onClick={this.handlePrev} className="btn prev" >PREV.</button>
-                            <span>Page : {this.state.page} ... {this.state.maxPage}</span>
-                            <button disabled ={this.state.page >= this.state.maxPage} onClick={this.handleNext} className="btn next" >NEXT</button>
-                        </div>
-                     </div>
-                    : <h4 className="loading">Loading...</h4>
+                        </InfiniteScroll>
+                    </div>
+                    : <Spinner />
                 }
             </>
         );
---
+++ src/components/Spinner.jsx
@@ -0,0 +1,14 @@
+import React,{Component} from "react";
+import "./Spinner.css";
+
+class Spinner extends Component{
+    render(){
+        return (
+            <>
+                <div class="loader"></div>
+            </>
+        );
+    }
+}
+
+export default Spinner;
